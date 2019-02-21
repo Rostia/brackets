afe8cf4e1a5c18953147c2a7764894fa1e72ae9d
Rostia <habupuc@gmail.com>
2019-02-21 16:38:03 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var stack = [];
+    for(var i = 0 ; i < str.length ; i++){
+        if(getOpenBrackets(str[i],bracketsConfig) !=='' && stack.length > 0 && stack[stack.length-1] === getOpenBrackets(str[i],bracketsConfig)){
+            stack.pop();
+        }else{
+            stack.push(str[i]);
+        }
+    }
+    return (stack.length > 0) ? false : true;
+}
+function getOpenBrackets(bracket, bracketsConfig) {
+    var openBracket = '';
+    for(var i = 0 ; i < bracketsConfig.length ; i++){
+        if(bracketsConfig[i][1] === bracket){
+            openBracket = bracketsConfig[i][0];
+            break;
+        }
+    }
+    return openBracket;
 }
